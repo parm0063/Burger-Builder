9b5cb9cb623a8c16d23461fbe06795892c5a2e40
Ruchita Parmar <parm0063@algonquinlive.com>
2021-03-18 14:28:53 -0400

    react-burger

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import Aux from '../../hoc/Auxx';
+
+class BurgerBuilder extends Component {
+    render() {
+        return(
+            <Aux>
+                <div>Burger</div>
+                <div>Build Controls</div>
+            </Aux>
+
+        )
+    }
+
+}
+
+export default BurgerBuilder;
